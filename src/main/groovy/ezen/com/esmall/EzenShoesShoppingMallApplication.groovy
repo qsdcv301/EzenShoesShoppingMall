68755e6b2ffbe1c61387e63799e1287c8bcef990
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-09-23 16:53:30 +0900

    프로젝트 초기설정
    스프링 boot환경을 위한 플러그인 추가

---
+++ src/main/groovy/ezen/com/esmall/EzenShoesShoppingMallApplication.groovy
@@ -0,0 +1,13 @@
+package ezen.com.esmall
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EzenShoesShoppingMallApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EzenShoesShoppingMallApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ezen/com/esmall/EzenShoesShoppingMallApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ezen.com.esmall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EzenShoesShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
