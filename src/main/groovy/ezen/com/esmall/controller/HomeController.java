5d6d18a8c2a71d2668f0e17ffffac7ed5a40f037
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-09-23 17:46:51 +0900

    fix/프로젝트 셋팅 수정
    프로젝트 셋팅 수정중

--- src/main/groovy/ezen/com/esmall/controller/HomeController.java
+++ src/main/groovy/ezen/com/esmall/controller/HomeController.java
@@ -2,9 +2,16 @@ package ezen.com.esmall.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
 
+    @GetMapping("/home")
+    public String home() {
+
+        return "templates/home.html";
+    }
 }
