6b56056e15ac2538ef06d50aaeadc8798f36d2e9
qsdcv301 <qsdcv301@naver.com>
2024-10-06 02:24:33 +0900

    db연결 테스트
    성공

--- src/main/groovy/ezen/com/esmall/controller/PageController.java
+++ src/main/groovy/ezen/com/esmall/controller/PageController.java
@@ -1,16 +1,22 @@
 package ezen.com.esmall.controller;
 
+import ezen.com.esmall.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class PageController {
 
-    @GetMapping("/home")
-    public String home() {
+    @Autowired
+    private UserService userService;
 
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("user1", userService.findById(1L));
         return "home";
     }
 
